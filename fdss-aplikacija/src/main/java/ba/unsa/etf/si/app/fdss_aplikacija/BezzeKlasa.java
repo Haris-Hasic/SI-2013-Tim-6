e1e97b4e52641b8e3d74340b6e67974452815a57
jasminkaldzija <jkaldzija1@etf.unsa.ba>
2014-05-13 00:16:40 +0200

    Brisanje testnih klasa

--- fdss-aplikacija/src/main/java/ba/unsa/etf/si/app/fdss_aplikacija/BezzeKlasa.java
+++
@@ -1,8 +0,0 @@
-package ba.unsa.etf.si.app.fdss_aplikacija;
-
-public class BezzeKlasa {
-	public void ispisi()
-	{
-		System.out.print("prelosa klasa");
-	}
-}
--- fdss-aplikacija/src/main/java/ba/unsa/etf/si/app/fdss_aplikacija/klasatest.java
+++
@@ -1,5 +0,0 @@
-package ba.unsa.etf.si.app.fdss_aplikacija;
-
-public class klasatest {
-
-}
