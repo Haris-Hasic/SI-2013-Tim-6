57d846b162e79488dae3b501fa999eeec05b80eb
enaganilovic <eganilovic1@etf.unsa.ba>
2014-05-30 18:17:46 +0200

    KlijentTest

--- fdss-aplikacija/src/test/java/ba/unsa/etf/si/app/fdss_aplikacija/beansTest/KlijentTest.java
+++ fdss-aplikacija/src/test/java/ba/unsa/etf/si/app/fdss_aplikacija/beansTest/KlijentTest.java
@@ -1,12 +1,14 @@
 package ba.unsa.etf.si.app.fdss_aplikacija.beansTest;
 
 
+import org.junit.Assert;
+import org.junit.Test;
+
 import junit.framework.TestCase;
 
 
-import static org.junit.Assert.*;
 
-import org.junit.Test;
+
 
 import ba.unsa.etf.si.app.fdss_aplikacija.beans.Klijent;
 import ba.unsa.etf.si.app.fdss_aplikacija.beans.Uposlenik;
@@ -29,5 +31,73 @@ public void testGetSetJmbgIspravno() {
 			fail("Test neuspješan. Ne rade get i set metode za atribut Jib.");
 		}
 	}
+
+
+public void testGetSetJibNeispravno() {
+	
+	Klijent k = new Klijent();
+	
+	try {
+		
+		k = new Klijent("Firma", "1607991186528", "Dervisa Susica 2", "Brcko", "+38761579652", "ena_brcko@hotmail.com","firma.ba");
+		k.setJib("1607");
+		
+		Assert.fail("Test neuspješan. Ne rade get i set metode za atribut jmbg.");
+	} 
+	
+	catch (GeneralniException e) {
+		
+		Assert.assertEquals("1607991186528", k.getJib());
+	}
+}
+
+ void testGetSetEmail() {
+	
+	 Klijent k = new Klijent();
+	 
+	try {
+		
+		k = new Klijent("Firma", "1607991186528", "Dervisa Susica 2", "Brcko", "+38761579652", "ena_brcko@hotmail.com","firma.ba");
+		k.setEmail("eganilovic1@etf.unsa.ba");
+		
+		Assert.assertEquals("eganilovic1@etf.unsa.ba", k.getEmail());
+	} 
+	
+	catch (GeneralniException e) {
+		
+		Assert.fail("Test neuspješan. Ne rade get i set metode za atribut e-mail.");
+	}
+}
+
+@Test
+public void testUpdateUposlenika() {
+	
+	Klijent k = new Klijent();
+	
+	try {
+		
+		k = new Klijent("Firma", "1607991186528", "Dervisa Susica 2", "Brcko", "+38761579652", "ena_brcko@hotmail.com","firma.ba");
+		k.setAdresa("Trg djece Dobrinje");
+		k.setMjesto("Sarajevo");
+		k.setNaziv("Firma");
+		k.setJib("2706992172174");
+		k.setEmail("firma@hotmail.com");
+		k.setTelefon("+38761503098");
+		k.setWeb("firma.com");
+	
+		
+		Boolean flag=false;
+		if (k.getAdresa()=="Trg djece Dobrinje" && k.getMjesto()=="Sarajevo" && k.getNaziv()=="Firma"  && k.getJib()=="2706992172174" && k.getEmail()=="firma@hotmail.com" && k.getTelefon()=="+38761503098")
+			flag=true;
+		
+		Assert.assertTrue(flag);
+	} 
+	
+	catch (GeneralniException e) {
+		
+		Assert.fail("Test neuspješan. Ne radi update-ovanje klijenta.");
+	}
+}
+
 
 }
