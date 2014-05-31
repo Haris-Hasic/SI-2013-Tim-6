81c89daa7315fa1a4060ac834225b86ecd5b8ef2
enaganilovic <eganilovic1@etf.unsa.ba>
2014-05-31 13:52:10 +0200

    ValidacijaTestovi zavrseno

--- fdss-aplikacija/src/test/java/ba/unsa/etf/si/app/fdss_aplikacija/beansTest/ValidacijaTestovi.java
+++ fdss-aplikacija/src/test/java/ba/unsa/etf/si/app/fdss_aplikacija/beansTest/ValidacijaTestovi.java
@@ -7,14 +7,36 @@ import org.junit.Test;
 import ba.unsa.etf.si.app.fdss_aplikacija.klase.Validacija;
 
 public class ValidacijaTestovi {
-
+	
+	Validacija v = new Validacija();
+	
 	@Test
 	public void ValidirajJmbg() {
-		Validacija v = new Validacija();
-		assertEquals(true,v.validirajJMBG("1607991186528"));
+	    
+		boolean validan = Validacija.validirajEmail("email@hotmail.com");
+		assertTrue(validan);
 	}
-
+	@Test
+	public void ValidirajIbfm() {
+		
+		assertTrue(Validacija.validirajIBFM("12345678"));
+	}
+	@Test
+	public void ValidirajIbfu() {
+		
+		assertEquals(true,Validacija.validirajIBFU("12345670"));
+	}
 	
+	@Test
+	public void ValidirajEmail() {
 	
+	assertEquals(true,Validacija.validirajEmail("eganilovic1@hotmail.com"));
+}
+	
+	@Test
+	public void ValidirajTelefon() {
+	
+	assertEquals(true,Validacija.validirajTelefon("+3876157965"));
+}
 
 }
